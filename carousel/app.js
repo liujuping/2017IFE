3b514311e75b085cd1c0cd1643962eedcf70e93c
刘菊萍 <liujuping@meituan.com>
2017-11-05 11:00:38 +0800

    fix overflow x

--- carousel/app.js
+++ carousel/app.js
@@ -5,5 +5,5 @@
 new Carousel({
   el: "carousel",
   type: "img",
-  imgs: ['imgs/1.jpg', 'imgs/2.jpg', 'imgs/3.jpg', 'imgs/4.jpg', 'imgs/5.jpg', 'imgs/6.jpg', 'imgs/7.jpg']
+  imgs: ['imgs/1.jpg', 'imgs/2.jpg', 'imgs/3.jpg', 'imgs/4.jpg', 'imgs/5.jpg', 'imgs/6.jpg', 'imgs/7.jpg', 'imgs/8.jpg','imgs/9.jpg','imgs/10.jpg']
 })
--- carousel/carousel.js
+++ carousel/carousel.js
@@ -17,19 +17,21 @@ Carousel.prototype = {
       // 静态设置
       // 图片之间的间隔距离
       this.imgSpace = 0.7
-      // 透视点的距离
-      this.imgInstance = 1.6
 
       this.rotateY = 360 / this.imgs.length
 
       // 图片的宽度百分比
       this.widthPercent = this.getWidthPercent()
 
-      this.rotateIndex = 3
+      this.rotateIndex = 1
       this.width = this.widthPercent * this.CarouselDOM.clientWidth / 100
+
+      // 透视点的距离
+      this.transZ = this.getTransZ(this.rotateY)
+      this.imgInstance = this.transZ + 2/7 * this.width
+
       this.initCarouselDOM()
       this.initImage()
-      // this.rotateImage()
       this.rotateTimer = null
       this.autoRotateImage()
     }
@@ -41,23 +43,22 @@ Carousel.prototype = {
     var self = this
     this.rotateTimer = setTimeout(function () {
       self.rotateImage()
-      // self.autoRotateImage()
+      self.autoRotateImage()
     }, 2000)
   },
   rotateImage() {
-    this.CarouselStageDOM.style = 'width: 100%;transition: transform 1s;transform-style: preserve-3d;transform: rotateY(' + this.rotateIndex * this.rotateY +'deg)'
+    this.CarouselStageDOM.style = 'width: ' + this.width + 'px;transform: rotateY(' + this.rotateIndex * this.rotateY +'deg)'
     this.rotateIndex ++
   },
   initCarouselDOM: function () {
-    // 'perspective: ' + this.width * this.imgInstance + 'px;
-    this.baseCarouselDOMStyle = 'perspective: ' + this.width * this.imgInstance + 'px;transform-style: preserve-3d;'
+    this.baseCarouselDOMStyle = 'perspective: ' + this.imgInstance + 'px;transform-style: preserve-3d;'
     this.CarouselDOM.style = this.baseCarouselDOMStyle
   },
   initImage: function () {
-    var html = '<div id="carousel-stage" style="width: 100%;">',
+    var html = '<div id="carousel-stage" style="width:' + this.width + 'px">',
     rotateY = 360 / this.imgs.length,
     transZ = this.getTransZ(rotateY),
-    baseStyle = 'position: absolute;left: ' + (100 - this.widthPercent * this.imgSpace) / 2 + '%;top: 10px;display: block;width: ' + this.widthPercent * this.imgSpace + '%;">'
+    baseStyle = 'left: '+ this.width * (1 - this.imgSpace) / 2 +'px; width: ' + this.width * this.imgSpace + 'px;">'
     this.imgs.forEach(function (item, index) {
       html +=
         '<div class="carousel-item" style="transform: rotateY(' + (rotateY * index ) + 'deg) translateZ(' + transZ + 'px);' + baseStyle +
